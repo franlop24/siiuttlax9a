8ead1fb92c81fa458664f902363f16deb1a412db
Franlop24 <franlopbri@gmail.com>
2024-06-14 12:21:37 -0600

    02 Home App

--- siiuttlax/siiuttlax/settings.py
+++ siiuttlax/siiuttlax/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Local Apps
+    'apps.home.apps.HomeConfig',
 ]
 
 MIDDLEWARE = [
